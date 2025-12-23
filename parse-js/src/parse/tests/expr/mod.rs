100380637ff837d8b2b4e441fe6003d645fa5669
Wilson Lin <code@wilsonl.in>
2025-12-23 11:45:06 -0500

    Remove debug logging and test angle-bracket assertions

--- parse-js/src/parse/tests/expr/mod.rs
+++ parse-js/src/parse/tests/expr/mod.rs
@@ -1,3 +1,7 @@
+use crate::ast::node::Node;
+use crate::ast::expr::Expr;
+use crate::ast::type_expr::TypeEntityName;
+use crate::ast::type_expr::TypeExpr;
 use crate::lex::Lexer;
 use crate::parse::expr::pat::ParsePatternRules;
 use crate::parse::ParseCtx;
@@ -6,19 +10,63 @@ use crate::token::TT;
 use crate::util::test::evaluate_test_input_files;
 use serde_json::Value;
 
-fn parse_expr_and_serialize(input: String) -> Value {
-  let mut parser = Parser::new(Lexer::new(&input));
+fn parse_expr(input: &str) -> Node<Expr> {
+  let mut parser = Parser::new(Lexer::new(input));
   let ctx = ParseCtx {
     rules: ParsePatternRules {
       await_allowed: true,
       yield_allowed: true,
     },
   };
-  let node = parser.expr(ctx, [TT::Semicolon]).unwrap();
+  parser.expr(ctx, [TT::Semicolon]).unwrap()
+}
+
+fn parse_expr_and_serialize(input: String) -> Value {
+  let node = parse_expr(&input);
   serde_json::to_value(&node).unwrap()
 }
 
 #[test]
 fn test_parse_expression() {
   evaluate_test_input_files("parse/tests/expr", parse_expr_and_serialize);
+}
+
+#[test]
+fn parses_angle_bracket_type_assertion_expression() {
+  let expr = parse_expr("<Foo>bar;");
+
+  match *expr.stx {
+    Expr::TypeAssertion(ref assertion) => {
+      assert!(!assertion.stx.const_assertion);
+
+      let type_annotation = assertion
+        .stx
+        .type_annotation
+        .as_ref()
+        .expect("expected type annotation for assertion");
+      match *type_annotation.stx {
+        TypeExpr::TypeReference(ref type_ref) => match &type_ref.stx.name {
+          TypeEntityName::Identifier(name) => assert_eq!(name, "Foo"),
+          other => panic!("expected identifier type name, got {:?}", other),
+        },
+        ref other => panic!("expected type reference, got {:?}", other),
+      }
+
+      match *assertion.stx.expression.as_ref().stx {
+        Expr::Id(ref id) => assert_eq!(id.stx.name, "bar"),
+        ref other => panic!("expected identifier expression, got {:?}", other),
+      }
+    }
+    ref other => panic!("expected type assertion expression, got {:?}", other),
+  }
+}
+
+#[test]
+fn parses_jsx_element_instead_of_type_assertion() {
+  let expr = parse_expr("<foo>bar</foo>;");
+
+  match *expr.stx {
+    Expr::JsxElem(_) => {}
+    ref other => panic!("expected JSX element, got {:?}", other),
+  }
 }
