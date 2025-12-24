bdb5a92a6ebc85295d540108459139fb552dd6fc
Wilson Lin <code@wilsonl.in>
2025-12-24 00:32:07 -0500

    Add minify README output parse test

--- minify-js/src/tests.rs
+++ minify-js/src/tests.rs
@@ -1,4 +1,5 @@
 use crate::minify;
+use parse_js::parse;
 use symbol_js::TopLevelMode;
 
 fn minified(mode: TopLevelMode, src: &str) -> String {
@@ -35,4 +36,11 @@ fn test_module_export_bindings_preserved() {
 fn test_export_function_inner_locals_renamed() {
   let result = minified(TopLevelMode::Module, "export function foo(){let bar=1;return bar;}");
   assert_eq!(result, "export function foo(){let a=1;return a;}");
+}
+
+#[test]
+fn readme_example_is_parseable() {
+  let code = "const main = () => { let my_first_variable = 1; };";
+  let result = minified(TopLevelMode::Global, code);
+  parse(&result).expect("minified output should be parseable JavaScript");
 }
