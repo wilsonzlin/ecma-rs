a2e5988337a9cf9317fffb313ecc5c4a3951ecdb
Wilson Lin <code@wilsonl.in>
2025-12-23 22:15:47 -0500

    Add boundary-aware token emitter

--- emit-js/src/lib.rs
+++ emit-js/src/lib.rs
@@ -1,6 +1,10 @@
+mod emitter;
 mod expr;
 mod expr_ts;
 mod ts_type;
 
+pub use emitter::EmitMode;
+pub use emitter::EmitOptions;
+pub use emitter::Emitter;
 pub use expr::{emit_expr, EmitError, EmitResult, ExprEmitter};
 pub use ts_type::{emit_ts_type, ts_type_to_string};
