574d088ef728e1672c84e1da0a6c89ab8514361b
Wilson Lin <code@wilsonl.in>
2025-12-24 19:26:26 -0500

    Make register allocation deterministic

--- optimize-js/src/analysis/registers.rs
+++ optimize-js/src/analysis/registers.rs
@@ -3,10 +3,13 @@ use ahash::HashMapExt;
 use ahash::HashSet;
 
 pub fn allocate_registers(intgraph: &HashMap<u32, HashSet<u32>>) -> HashMap<u32, u32> {
-  let all_colours = HashSet::from_iter(0..u32::try_from(intgraph.len()).unwrap());
   let mut rem = intgraph.clone();
   let mut stack = Vec::new();
-  while let Some(t) = rem.iter().min_by_key(|(_, c)| c.len()).map(|(t, _)| *t) {
+  while let Some(t) = rem
+    .iter()
+    .min_by(|(t1, c1), (t2, c2)| c1.len().cmp(&c2.len()).then_with(|| t1.cmp(t2)))
+    .map(|(t, _)| *t)
+  {
     stack.push(t);
     let children = rem.remove(&t).unwrap();
     for &c in children.iter() {
@@ -14,14 +17,15 @@ pub fn allocate_registers(intgraph: &HashMap<u32, HashSet<u32>>) -> HashMap<u32,
     }
   }
   let mut allocated = HashMap::<u32, u32>::new();
+  let max_colour = u32::try_from(intgraph.len()).unwrap();
   for t in stack.into_iter().rev() {
-    let mut avail = all_colours.clone();
+    let mut used = HashSet::default();
     for neighbour in intgraph[&t].iter() {
       if let Some(&neighbour_colour) = allocated.get(&neighbour) {
-        avail.remove(&neighbour_colour);
+        used.insert(neighbour_colour);
       };
     }
-    let pick = *avail.iter().next().unwrap();
+    let pick = (0..max_colour).find(|c| !used.contains(c)).unwrap();
     assert!(allocated.insert(t, pick).is_none());
   }
   allocated
