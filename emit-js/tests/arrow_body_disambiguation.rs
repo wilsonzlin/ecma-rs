439ef41554e80cc60bb484056f1ea0dec2a04b0e
Wilson Lin <code@wilsonl.in>
2025-12-24 19:24:54 -0500

    Add arrow body disambiguation tests

---
+++ emit-js/tests/arrow_body_disambiguation.rs
@@ -0,0 +1,53 @@
+use emit_js::emit_expr;
+use parse_js::ast::expr::Expr;
+use parse_js::ast::node::Node;
+use parse_js::ast::type_expr::TypeExpr;
+use parse_js::lex::Lexer;
+use parse_js::parse::expr::pat::ParsePatternRules;
+use parse_js::parse::{ParseCtx, Parser};
+use parse_js::token::TT;
+
+fn parse_expression(input: &str) -> Node<Expr> {
+  let mut parser = Parser::new(Lexer::new(input));
+  let ctx = ParseCtx {
+    rules: ParsePatternRules {
+      await_allowed: true,
+      yield_allowed: true,
+    },
+  };
+  let expr = parser.expr(ctx, [TT::EOF]).expect("parse expression");
+  parser.require(TT::EOF).expect("exhaust input");
+  expr
+}
+
+fn emit_expression(expr: &Node<Expr>) -> String {
+  let mut out = String::new();
+  let mut emit_type = |_out: &mut String, _ty: &Node<TypeExpr>| Ok(());
+  emit_expr(&mut out, expr, &mut emit_type).expect("emit expression");
+  out
+}
+
+fn assert_roundtrip(input: &str, expected: &str) {
+  let parsed = parse_expression(input);
+  let emitted = emit_expression(&parsed);
+  assert_eq!(emitted, expected);
+
+  let reparsed = parse_expression(&emitted);
+  let reemitted = emit_expression(&reparsed);
+  assert_eq!(reemitted, expected);
+}
+
+#[test]
+fn parenthesizes_object_literal_bodies() {
+  assert_roundtrip("x=>({a:1})", "(x) => ({a: 1})");
+}
+
+#[test]
+fn parenthesizes_comma_bodies() {
+  assert_roundtrip("x=>(a,b)", "(x) => (a, b)");
+}
+
+#[test]
+fn parenthesizes_arrow_in_member_chain() {
+  assert_roundtrip("(x=>({a:1})).prop", "((x) => ({a: 1})).prop");
+}
