b0716194a6282ba09d38206e11202bea8234ff04
Wilson Lin <code@wilsonl.in>
2026-01-01 08:50:42 +0000

    chore(fmt): rustfmt types-ts-interned eval
    Format recent types-ts-interned template literal evaluation change so  stays clean.

--- types-ts-interned/src/eval.rs
+++ types-ts-interned/src/eval.rs
@@ -1134,10 +1134,12 @@ impl<'a, E: TypeExpander> TypeEvaluator<'a, E> {
             ty: self.evaluate_with_subst(chunk.ty, subst, depth + 1),
           })
           .collect();
-        self.store.intern_type(TypeKind::TemplateLiteral(TemplateLiteralType {
-          head: tpl.head,
-          spans: evaluated_spans,
-        }))
+        self
+          .store
+          .intern_type(TypeKind::TemplateLiteral(TemplateLiteralType {
+            head: tpl.head,
+            spans: evaluated_spans,
+          }))
       }
     }
   }
