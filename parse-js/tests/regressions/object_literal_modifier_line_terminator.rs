76fcc7b3ecfbc36e65cb4bc353549d2e1855e10b
Wilson Lin <code@wilsonl.in>
2026-01-07 15:06:12 +0000

    test(parse-js): extend object-literal modifier newline regressions
    - Run object-literal modifier newline tests under Dialect::Ecma
    - Add negative case for set followed by a line terminator

--- parse-js/tests/regressions/object_literal_modifier_line_terminator.rs
+++ parse-js/tests/regressions/object_literal_modifier_line_terminator.rs
@@ -1,18 +1,23 @@
 use parse_js::{parse_with_options, Dialect, ParseOptions, SourceType};
 
-fn js_script_opts() -> ParseOptions {
+fn ecma_script_opts() -> ParseOptions {
   ParseOptions {
-    dialect: Dialect::Js,
+    dialect: Dialect::Ecma,
     source_type: SourceType::Script,
   }
 }
 
 #[test]
 fn object_literal_async_modifier_must_not_cross_line_terminator() {
-  assert!(parse_with_options("({ async\nfoo(){} })", js_script_opts()).is_err());
+  assert!(parse_with_options("({ async\nfoo(){} })", ecma_script_opts()).is_err());
 }
 
 #[test]
 fn object_literal_get_modifier_must_not_cross_line_terminator() {
-  assert!(parse_with_options("({ get\nfoo(){} })", js_script_opts()).is_err());
+  assert!(parse_with_options("({ get\nfoo(){} })", ecma_script_opts()).is_err());
+}
+
+#[test]
+fn object_literal_set_modifier_must_not_cross_line_terminator() {
+  assert!(parse_with_options("({ set\nfoo(v){} })", ecma_script_opts()).is_err());
 }
