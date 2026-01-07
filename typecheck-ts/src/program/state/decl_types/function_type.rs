634adb7bb19b876ab837dd3a47382e06ec99bb8a
Wilson Lin <code@wilsonl.in>
2026-01-07 08:47:57 +0000

    refactor(typecheck-ts): split decl type computation into submodules
    Move ProgramState::type_of_def and related helpers out of state/decl_types.rs into state/decl_types/{type_of_def,function_type}.rs. type_of_def keeps state-module visibility via pub(in super::super); function helpers are pub(super) within the decl_types module.

---
+++ typecheck-ts/src/program/state/decl_types/function_type.rs
@@ -0,0 +1,183 @@
+use super::*;
+
+impl ProgramState {
+  pub(super) fn infer_cached_callable_return_type(
+    &mut self,
+    def: DefId,
+    func: &FuncData,
+    store: &Arc<tti::TypeStore>,
+    callable_ty: TypeId,
+  ) -> Result<Option<TypeId>, FatalError> {
+    let Some(body) = func.body else {
+      return Ok(None);
+    };
+    let is_async = self.body_is_async_function(body);
+    let tti::TypeKind::Callable { overloads } = store.type_kind(callable_ty) else {
+      return Ok(None);
+    };
+    if overloads.len() != 1 {
+      return Ok(None);
+    }
+    let mut ret = if self.checking_bodies.contains(&body) {
+      store.primitive_ids().unknown
+    } else {
+      let res = self.check_body(body)?;
+      if res.return_types.is_empty() {
+        store.primitive_ids().void
+      } else {
+        let mut members = Vec::new();
+        for ty in res.return_types.iter() {
+          let ty = store.canon(self.ensure_interned_type(*ty));
+          let widened = check::widen::widen_literal(store.as_ref(), ty);
+          members.push(widened);
+        }
+        store.union(members)
+      }
+    };
+    let prim = store.primitive_ids();
+    if is_async && ret != prim.unknown {
+      ret = self
+        .promise_ref(store.as_ref(), ret)
+        .unwrap_or(prim.unknown);
+    }
+    let sig_id = overloads[0];
+    let mut sig = store.signature(sig_id);
+    if sig.ret == ret {
+      return Ok(None);
+    }
+    sig.ret = ret;
+    let sig_id = store.intern_signature(sig);
+    let callable_ty = store.canon(store.intern_type(tti::TypeKind::Callable {
+      overloads: vec![sig_id],
+    }));
+    self.interned_def_types.insert(def, callable_ty);
+    self.def_types.insert(def, callable_ty);
+    if let Some(def_data) = self.def_data.get(&def) {
+      if let Some(file_state) = self.files.get_mut(&def_data.file) {
+        if let Some(binding) = file_state.bindings.get_mut(&def_data.name) {
+          if binding.def == Some(def) {
+            binding.type_id = Some(callable_ty);
+          }
+        }
+      }
+    }
+    Ok(Some(callable_ty))
+  }
+
+  fn body_is_async_function(&self, body_id: BodyId) -> bool {
+    let Some(meta) = self.body_map.get(&body_id) else {
+      return false;
+    };
+    let Some(hir_body_id) = meta.hir else {
+      return false;
+    };
+    let Some(lowered) = self.hir_lowered.get(&meta.file) else {
+      return false;
+    };
+    let Some(body) = lowered.body(hir_body_id) else {
+      return false;
+    };
+    body.function.as_ref().is_some_and(|f| f.async_)
+  }
+
+  fn resolve_promise_def(&self) -> Option<tti::DefId> {
+    let mut best: Option<((u8, u8, u32, u32, u64), DefId)> = None;
+    for (def, data) in self.def_data.iter() {
+      if data.name != "Promise" {
+        continue;
+      }
+      let pri = self.def_priority(*def, sem_ts::Namespace::TYPE);
+      if pri == u8::MAX {
+        continue;
+      }
+      let origin = self.file_registry.lookup_origin(data.file);
+      let origin_rank: u8 = if self.current_file == Some(data.file) {
+        0
+      } else if matches!(origin, Some(FileOrigin::Source)) {
+        1
+      } else {
+        2
+      };
+      let span = data.span;
+      let key = (origin_rank, pri, span.start, span.end, def.0);
+      best = match best {
+        Some((best_key, best_def)) if best_key <= key => Some((best_key, best_def)),
+        _ => Some((key, *def)),
+      };
+    }
+    best.map(|(_, def)| tti::DefId(def.0))
+  }
+
+  fn promise_ref(&self, store: &tti::TypeStore, inner: TypeId) -> Option<TypeId> {
+    let def = self.resolve_promise_def()?;
+    Some(store.canon(store.intern_type(tti::TypeKind::Ref {
+      def,
+      args: vec![store.canon(inner)],
+    })))
+  }
+
+  pub(super) fn function_type(&mut self, def: DefId, func: FuncData) -> Result<TypeId, FatalError> {
+    self.check_cancelled()?;
+    let param_types: Vec<TypeId> = func
+      .params
+      .iter()
+      .map(|p| p.typ.unwrap_or(self.builtin.any))
+      .collect();
+    let inferred_from_body = func.return_ann.is_none() && func.body.is_some();
+    let ret = if let Some(ret) = func.return_ann {
+      ret
+    } else if let Some(body) = func.body {
+      self.check_cancelled()?;
+      if self.checking_bodies.contains(&body) {
+        self.builtin.unknown
+      } else {
+        let res = self.check_body(body)?;
+        if res.return_types.is_empty() {
+          self.builtin.void
+        } else {
+          let mut widened = Vec::new();
+          for ty in res.return_types.iter() {
+            let imported = self.import_interned_type(*ty);
+            widened.push(self.widen_literal(imported));
+          }
+          self.type_store.union(widened, &self.builtin)
+        }
+      }
+    } else {
+      self.builtin.unknown
+    };
+    let ty = self.type_store.function(param_types, ret);
+    if let Some(store) = self.interned_store.as_ref() {
+      let mut cache = HashMap::new();
+      let mut interned = store.canon(convert_type_for_display(ty, self, store, &mut cache));
+      if inferred_from_body
+        && func
+          .body
+          .is_some_and(|body| self.body_is_async_function(body))
+      {
+        let prim = store.primitive_ids();
+        if let tti::TypeKind::Callable { overloads } = store.type_kind(interned) {
+          if overloads.len() == 1 {
+            let sig_id = overloads[0];
+            let mut sig = store.signature(sig_id);
+            if sig.ret != prim.unknown {
+              let wrapped = self
+                .promise_ref(store.as_ref(), sig.ret)
+                .unwrap_or(prim.unknown);
+              if wrapped != sig.ret {
+                sig.ret = wrapped;
+                let sig_id = store.intern_signature(sig);
+                interned = store.canon(store.intern_type(tti::TypeKind::Callable {
+                  overloads: vec![sig_id],
+                }));
+              }
+            }
+          }
+        }
+      }
+      self.interned_def_types.insert(def, interned);
+    }
+    self.def_types.insert(def, ty);
+    Ok(ty)
+  }
+}
